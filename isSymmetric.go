6f5af156fbeca85790d37b8f5ee4666e66fb1838
Daniel1147 <sxn91401+github@gmail.com>
2017-10-08 07:03:44 +0000

    better symmetric

--- isSymmetric.go
+++ isSymmetric.go
@@ -13,46 +13,32 @@ func isSymmetric(root *TreeNode) bool {
     } else if root.Left == nil || root.Right == nil {
         return false
     }
-    leftDump := dump(root.Left, 'L')
-    rightDump := dump(root.Right, 'R')
 
-    return sliceEqual(leftDump, rightDump)
+    return symmetric(root.Left, root.Right)
 }
 
-func sliceEqual(s1, s2 []int) bool {
-    if len(s1) != len(s2) {
+func symmetric(left *TreeNode, right *TreeNode) bool {
+    if (left == nil && right == nil) {
+        return true
+    }
+
+    if (left == nil || right == nil) {
         return false
     }
 
-    for i := 0; i < len(s1); i++ {
-        if (s1[i] != s2[i]) {
-            return false
-        }
+    if (left.Val != right.Val) {
+        return false
     }
+
+    if (symmetric(left.Left, right.Right) != true) {
+        return false
+    }
+
+    if (symmetric(left.Right, right.Left) != true) {
+        return false
+    }
+
     return true
-}
-
-func dump(node *TreeNode, direction byte) []int {
-    var left, right, middle []int
-    if (node.Left != nil) {
-        left = dump(node.Left, direction)
-    } else {
-        left = []int{-1}
-    }
-
-    middle = []int{node.Val}
-
-    if (node.Right != nil) {
-        right = dump(node.Right, direction)
-    } else {
-        right = []int{-1}
-    }
-
-    if direction == 'L' {
-        return append(middle, append(left, right...)...)
-    } else {
-        return append(middle, append(right, left...)...)
-    }
 }
 
 func main() {
