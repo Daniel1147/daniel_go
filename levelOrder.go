d8c6d7379df7f0f1f5a63792f2105987a98341e7
Daniel1147 <sxn91401+github@gmail.com>
2017-10-08 14:05:15 +0000

    finish levelOrder

---
+++ levelOrder.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+import . "./myApp"
+
+var ansList [][]int
+
+func levelOrder(root *TreeNode) [][]int {
+    ansList = make([][]int, 0)
+    addNode(root, 0)
+
+    return ansList
+}
+
+func addNode(node *TreeNode, level int) {
+    if node == nil {
+        return
+    }
+
+    if len(ansList) <= level {
+        ansList = append(ansList, []int{})
+    }
+
+    addNode(node.Left, level + 1)
+    addNode(node.Right, level + 1)
+
+    ansList[level] = append(ansList[level], node.Val)
+
+    return
+}
+
+func main() {
+    treeNode := Sample()
+    treeNode.Show()
+    fmt.Println(levelOrderBottom(treeNode))
+
+	return
+}
+
