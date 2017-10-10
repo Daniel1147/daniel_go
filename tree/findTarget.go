d3292612fb289e47f22dcb1c24047b5c60734711
Daniel1147 <sxn91401+github@gmail.com>
2017-10-10 10:42:50 +0000

    using new algorithm

--- tree/findTarget.go
+++ tree/findTarget.go
@@ -3,75 +3,30 @@ package tree
 // import "fmt"
 import . "github.com/Daniel1147/tree"
 
-var nillNum = -1147
+var nums []int
 
 func findTarget(root *TreeNode, k int) bool {
-    if root == nil {
-        return false
-    }
-    result := searchBST(root, k - root.Val, root.Val)
-    if result == true {
-        return true
-    }
-
-    leftResult := findTargetHelper(root.Left, k, root)
-    if leftResult == true {
-        return true
-    }
-
-    rightResult := findTargetHelper(root.Right, k, root)
-    if rightResult == true {
-        return true
+    nums = make([]int, 0)
+    buildSliceFromTree(root)
+    remainMap := make(map[int]int)
+    for index, value := range nums {
+        remain := k - value
+        _, exist := remainMap[remain]
+        if (exist) {
+            return true
+        }
+        remainMap[value] = index
     }
 
     return false
 }
 
-func findTargetHelper(node *TreeNode, k int, root *TreeNode) bool {
-    if node == nil {
-        return false
-    }
-    result := searchBST(root, k - node.Val, node.Val)
-    if result == true {
-        return true
-    }
-
-    if k <= node.Val {
-        leftResult := findTargetHelper(node.Left, k, root)
-        if leftResult == true {
-            return true
-        }
-    }
-
-    if k >= node.Val {
-        rightResult := findTargetHelper(node.Right, k, root)
-        if rightResult == true {
-            return true
-        }
-    }
-
-    return false
-}
-
-func searchBST(root *TreeNode, targetNum int, usedNum int) bool {
+func buildSliceFromTree(root *TreeNode) {
     if (root == nil) {
-        return false
+        return
     }
 
-    if root.Val == usedNum {
-        usedNum = nillNum
-    } else {
-        if root.Val == targetNum {
-            return true
-        }
-    }
-
-    leftResult := searchBST(root.Left, targetNum, usedNum)
-    rightResult := searchBST(root.Right, targetNum, usedNum)
-
-    if leftResult == true || rightResult == true {
-        return true
-    }
-
-    return false
+    buildSliceFromTree(root.Left)
+    nums = append(nums, root.Val)
+    buildSliceFromTree(root.Right)
 }
