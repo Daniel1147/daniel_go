746b1fbf5bd04961a02f66d58f2af4bc348eb11d
Daniel1147 <sxn91401+github@gmail.com>
2017-10-10 05:10:17 +0000

    add test

---
+++ tree/findSecondMinimumValue_test.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+import . "github.com/Daniel1147/tree"
+import "testing"
+
+func TestLongestUnivaluePath(t *testing.T) {
+    cases := []struct {
+        root *TreeNode
+        expected int
+    } {
+        sample1(),
+    }
+
+    for _, testCase := range cases {
+        caseDump := testCase.root.Dump()
+        // get result
+        result := findSecondMinimumValue(testCase.root)
+
+        // build expected
+        expected := testCase.expected
+
+        // judge
+        if (expected != result) {
+            errorMsg := fmt.Sprintf(
+                "\ntestCase %v\nexpected %v\nresult %v",
+                caseDump,
+                expected,
+                result,
+            )
+            t.Errorf(errorMsg)
+        }
+    }
+}
+
+func sample1() (result struct {
+    root *TreeNode
+    expected int
+}) {
+    root := NewTreeNode(2)
+    root.Left = NewTreeNode(2)
+    root.Right = NewTreeNode(5)
+    root.Right.Left = NewTreeNode(5)
+    root.Right.Right = NewTreeNode(7)
+
+    result.root = root
+    result.expected = 5
+    return
+}
