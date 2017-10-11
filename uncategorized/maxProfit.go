e4fc2f6c7e38cd20ad22ad5a2e41d882d787eb8a
Daniel1147 <sxn91401+github@gmail.com>
2017-10-11 14:38:01 +0000

    add a little imporve

--- uncategorized/maxProfit.go
+++ uncategorized/maxProfit.go
@@ -3,7 +3,7 @@ package uncategorized
 func maxProfit(prices []int) int {
     var max int
     for i, p := range prices {
-        for _, n := range prices[i:] {
+        for _, n := range prices[i + 1:] {
             if n - p > max {
                 max = n - p
             }
