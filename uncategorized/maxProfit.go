e7ddc63334e9cff5bb3a6a1a9ffa2d90d735e810
Daniel1147 <sxn91401+github@gmail.com>
2017-10-29 14:55:14 +0000

    [optimize] imporve performance by removing variable declaration

--- uncategorized/maxProfit.go
+++ uncategorized/maxProfit.go
@@ -27,20 +27,18 @@ func maxProfit2(prices []int) int {
         return profitSum
     }
 
-    lastPrice := prices[0]
     minPrice := prices[0]
     for i := 1; i < priceNum; i++ {
-        currentPrice := prices[i]
-        if (currentPrice < lastPrice) {
-            profitSum += lastPrice - minPrice
-            minPrice = currentPrice
+        // prices[i] as current price
+        // prices[i - 1] as last price
+        if (prices[i] < prices[i - 1]) {
+            profitSum += prices[i - 1] - minPrice
+            minPrice = prices[i]
         } else {
         }
-
-        lastPrice = currentPrice
     }
 
-    profitSum += lastPrice - minPrice
+    profitSum += prices[len(prices) - 1] - minPrice
 
     return profitSum
 }
